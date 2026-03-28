18f0821008e2eee6cb981fab4c78efdb123176e8
Angie <angiesofiahh@gmail.com>
2026-03-27 21:20:35 -0500

    Implementa VistaDocente para mostrar detalles y lista de docentes

---
+++ src/main/java/com/uniajc/vista/VistaDocente.java
@@ -0,0 +1,21 @@
+package com.uniajc.vista;
+
+import java.util.List;
+import com.uniajc.modelo.Docente;
+
+public class VistaDocente {
+
+	public void mostrarDetallesDocente(Docente docente) {
+		System.out.println("Detalles del Docente:");
+		System.out.println("Nombre: " + docente.getNombre());
+		System.out.println("Especialidad: " + docente.getEspecialidad());
+	}
+
+	public void mostrarTodosLosDocentes(List<Docente> docentes) {
+		System.out.println("Lista de Docentes:");
+		for (Docente docente : docentes) {
+			mostrarDetallesDocente(docente);
+			System.out.println("-------------------");
+		}
+	}
+}
