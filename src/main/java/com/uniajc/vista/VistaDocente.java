086847f98422075ad53599e3e3ab6bc0638a23e5
Samir-Hurtado <hurtadosamir20@gmail.com>
2026-04-07 19:20:32 -0500

    modificando vista docente

--- src/main/java/com/uniajc/vista/VistaDocente.java
+++ src/main/java/com/uniajc/vista/VistaDocente.java
@@ -1,5 +1,29 @@
 package com.uniajc.vista;
+import java.util.List;
+
+import com.uniajc.modelo.Docente;
 
 public class VistaDocente {
+
+    public void mostrarDetallesDocente(Docente docente) {
+        System.out.println("Detalles del Docente:");
+        System.out.println("Nombre: " + docente.getNombre());
+        System.out.println("Edad: " + docente.getEdad());
+        System.out.println("Materia: " + docente.getMateria());
+        System.out.println("Salario: " + docente.getSalario());
+        
+
+
+    }
+
+    public void mostrarTodosLosDocentes(List<Docente> docentes) {
+        System.out.println("Lista de Docentes:");
+        for (Docente docente : docentes) {
+            mostrarDetallesDocente(docente);
+            System.out.println("-------------------");
+        }
+    }
+
+    
 
 }
