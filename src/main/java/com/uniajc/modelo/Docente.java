17b0f5c60305495d5ed82ed015a3c561fe8cecb4
Samir-Hurtado <hurtadosamir20@gmail.com>
2026-04-07 19:18:53 -0500

    agregando nuevos atributos y metodos a la clase docente

--- src/main/java/com/uniajc/modelo/Docente.java
+++ src/main/java/com/uniajc/modelo/Docente.java
@@ -1,5 +1,57 @@
 package com.uniajc.modelo;
 
 public class Docente {
+    public String nombre;
+    private int edad;
+    private String materia; // Nueva propiedad para la materia que enseña el docente
+    private double salario; // Nueva propiedad para el salario del docente
+    
+
+    public Docente(String nombre, int edad, String materia, double salario) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.materia = materia;
+        this.salario = salario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getMateria() {
+        return materia;
+    }
+
+    public void setMateria(String materia) {
+        this.materia = materia;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public void aumentarSalario(double porcentaje) {
+        salario += salario * porcentaje / 100;
+    }
+
+    public String mostrarResumen() {
+        return nombre + " enseña " + materia;
+    }
 
 }
