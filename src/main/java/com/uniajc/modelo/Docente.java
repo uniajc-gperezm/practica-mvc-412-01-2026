caca5324ebeb220d7d190f3229bcb7d94173836d
CristianErazo-hub <cristianerazo420@gmail.com>
2026-04-10 19:03:14 -0500

    Agregando código correspondiente para cada clase

---
+++ src/main/java/com/uniajc/modelo/Docente.java
@@ -0,0 +1,27 @@
+package com.uniajc.modelo;
+
+public class Docente {
+    String nombre;
+    int edad;
+
+    public Docente(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+}
--- src/main/java/com/uniajc/modelo/ModeloDocente.java
+++
@@ -1,5 +0,0 @@
-package com.uniajc.modelo;
-
-public class ModeloDocente {
-
-}
