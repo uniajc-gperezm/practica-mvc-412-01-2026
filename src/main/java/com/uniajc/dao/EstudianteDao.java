1eb2b00653723791e67eca3d5ff057b6124d956f
JeanRojas17 <jeanpaulrojas17@gmail.com>
2026-04-14 22:23:18 -0500

    Corrigiendo el archivo DAO

--- src/main/java/com/uniajc/dao/EstudianteDao.java
+++ src/main/java/com/uniajc/dao/EstudianteDao.java
@@ -14,68 +14,6 @@ import com.uniajc.config.ConexionPostgresDatabase;
 import com.uniajc.modelo.Estudiante;
 
 public class EstudianteDao {
-    // guardarEstudiante(Estudiante estudiante)
-    // INSERT INTO "practica-mvc".estudiantes (name, lastname, email) VALUES ('Gabriel', 'Perez', 'pepito@email.com');
-    public void guardarEstudiante(Estudiante estudiante) {
-        String sql = "INSERT INTO \"practica-mvc\".estudiantes (name, lastname, email) VALUES (?, ?, ?);";
-
-        try (Connection conn = ConexionPostgresDatabase.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-
-            pstmt.setString(1, estudiante.getNombre());
-            pstmt.setString(2, estudiante.getApellido());
-            pstmt.setString(3, estudiante.getCorreo());
-
-            
-            pstmt.executeUpdate();
-
-        } catch (SQLException error) {
-            error.printStackTrace();
-        }
-    }
-
-
-    // obtenerTodosLosEstudiantes()
-    // SELECT id, name, lastname, email FROM "practica-mvc".estudiantes;
-
-    public List<Estudiante> obtenerTodosLosEstudiantes() {
-        List<Estudiante> estudiantes = new ArrayList<Estudiante>();
-        
-        String sql = "SELECT id, name, lastname, email FROM \"practica-mvc\".estudiantes;";
-
-        try (Connection conn = ConexionPostgresDatabase.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql);
-            ResultSet rs = pstmt.executeQuery()) {
-
-            while (rs.next()) {
-
-                Estudiante estudiante = new Estudiante();
-
-                estudiante.setId(rs.getInt("id"));
-                estudiante.setNombre(rs.getString("name"));
-                estudiante.setApellido(rs.getString("lastname"));
-                estudiante.setCorreo(rs.getString("email"));
-
-                estudiantes.add(estudiante);
-            }
-
-        } catch (SQLException error) {
-            error.printStackTrace();
-        }
-
-        return estudiantes;
-    }
-
-    
-    // obtenerEstudiantePorId(int id)
-    // SELECT id, name, lastname, email FROM "practica-mvc".estudiantes WHERE id = 2;
-
-
-    // actualizarEstudiante(Estudiante estudiante)
-    // UPDATE "practica-mvc".estudiantes
-    // SET email = 'emailactualizado@example.com', lastname = 'nuevoapellido'
-    // WHERE id = 2;
-
 
     // guardarEstudiante(Estudiante estudiante)
     // INSERT INTO "practica-mvc".estudiantes (name, lastname, email) VALUES ('Gabriel', 'Perez', 'pepito@email.com');
