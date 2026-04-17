24a33d6f6c0f6c299957c39aa7f6fe2d3f3c0bd7
CristianErazo-hub <cristianerazo420@gmail.com>
2026-04-17 16:32:26 -0500

    Agregando eliminarEstudiante en service

--- src/main/java/com/uniajc/servicios/EstudianteService.java
+++ src/main/java/com/uniajc/servicios/EstudianteService.java
@@ -1,5 +1,44 @@
 package com.uniajc.servicios;
 
+import java.util.List;
+
+import com.uniajc.dao.EstudianteDao;
+import com.uniajc.modelo.Estudiante;
+
 public class EstudianteService {
 
+    private final EstudianteDao estudianteDao;
+
+    public EstudianteService(EstudianteDao estudianteDao) {
+        this.estudianteDao = estudianteDao;
+    }
+
+    public void registrarEstudiante(Estudiante estudiante) {
+
+        if(estudiante == null) {
+            throw new IllegalArgumentException("El estudiante no puede ser nulo.");
+        }
+
+        if(estudiante.getNombre() == null || estudiante.getApellido() == null) {
+            throw new IllegalArgumentException("Los campos nombre y apellido del estudiante son obligatorios.");
+        }
+
+        estudianteDao.guardarEstudiante(estudiante);
+    }
+
+    public List<Estudiante> mostrarTodosLosEstudiantes() {
+        return estudianteDao.obtenerTodosLosEstudiantes();
+    }
+
+    public Estudiante obtenerEstudiantePorId(int id) {
+        return estudianteDao.obtenerEstudiantePorId(id);
+    }
+
+    public void actualizarEstudiante(Estudiante estudiante) {
+        estudianteDao.actualizarEstudiante(estudiante);
+    }
+
+    public void eliminarEstudiante(int id) {
+        estudianteDao.eliminarEstudiante(id);
+    }
 }
